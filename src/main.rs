57cb6a8d6ca8834e39f35f747d83607a935ce65b
DeveloperGY <dev.garrettyenojr@gmail.com>
2023-09-09 18:07:16 -0400

    e

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+/// Flying Parrot Window Manager
+/// 
+/// Author: DeveloperGY
+/// License: GPL3
+/// 
+/// fpwm is a basic X11 window manager written in rust
+
 mod wm;
 
 use wm::WM;
 
 fn main() {
     let wm = WM::create();
+    wm.run();
 }
--- src/wm.rs
+++ src/wm.rs
@@ -1,31 +1,57 @@
 use x11::xlib::*;
 
+/// The use of static state is required due to how xlib is implemented
+
+/// Represents whether or not there is another window manager running, which
+/// would prevent fpwm from becoming the current window manager
+static mut CAN_ASCEND: bool = true;
+
 /// The core of fpwm. It represents an X11 Window manger.
 pub struct WM {
-    display: *mut Display
+    /// Main connection to the X server
+    display: *mut Display,
+
+    /// The root window
+    root: u64
 }
 
-/// 
+/// functions used to create the window manager
 impl WM {
-    pub fn new(display: *mut Display) -> Self {
+    fn new(display: *mut Display) -> Self {
+
         Self {
-            display
+            display,
+            root: unsafe {XDefaultRootWindow(display)}
         }
+
     }
 
     pub fn create() -> Self {
+
         let display = match WM::connect() {
             Ok(d) => d,
             Err(e) => panic!("{}", e)
         };
 
-        WM::new(display)
+        let wm = WM::new(display);
+
+        match wm.verify_wm_ascension() {
+            Ok(_) => (),
+            Err(e) => panic!("{}", e)
+        };
+
+
+
+        wm
+
     }
 }
 
 /// Helpers for creating the window manager
 impl WM {
+    /// Creates a connection to the X server
     fn connect() -> Result<*mut Display, &'static str> {
+
         unsafe {
             let display = XOpenDisplay(std::ptr::null());
 
@@ -36,5 +62,69 @@ impl WM {
                 Ok(display)
             }
         }
+
+    }
+
+    /// Verify that there is no other window manager currently in use
+    fn verify_wm_ascension(&self) -> Result<(), &'static str> {
+
+        unsafe {
+            XSetErrorHandler(Some(WM::handle_wm_ascension_error));
+            XSelectInput(
+                self.display,
+                self.root,
+                SubstructureRedirectMask | SubstructureNotifyMask
+            );
+            XSync(self.display, 0);
+            XSetErrorHandler(None); // TODO: Create an error handler
+        
+            if CAN_ASCEND {
+                Ok(())
+            }
+            else {
+                Err("Failed to ascend, another window manager is running")
+            }
+        }
+
+    }
+
+    /// Error Handler for if there is another window manager in use
+    extern "C" fn handle_wm_ascension_error(
+
+        display: *mut Display,
+        e: *mut XErrorEvent
+    
+    ) -> i32 {
+
+        unsafe {
+            match (*e).error_code {
+                BadAccess => {
+                    CAN_ASCEND = false;
+                }
+                _ => ()
+            };
+
+            0
+        }
+
+    }
+}
+
+/// Handlers for the execution of the window manager
+impl WM {
+    pub fn run(&self) {
+
+        unsafe {
+            let mut e: XEvent = std::mem::zeroed();
+
+            loop {
+                XNextEvent(self.display, &mut e);
+
+                match e.get_type() {
+                    _ => ()
+                }
+            }
+        }
+
     }
 }
